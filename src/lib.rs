d4e4ccf7a77289ecaddc88f3e2841e399c3c9953
Thomas Johnson <NTmatter@gmail.com>
2025-06-22 04:01:45 +0200

    feat: Derive macro to allow use inside enums

--- src/lib.rs
+++ src/lib.rs
@@ -10,26 +10,28 @@ use proc_macro::TokenStream;
 /// revision, and various other properties. Has no functionality, other than
 /// as a marker for external tools.
 ///
+/// Provides no functionality, and expands to nothing.
+///
 /// NOTE: This is not yet validated, as the fields have not been stabilized.
 ///
 /// # Arguments
-/// * `codebase` - Optional name of codebase in which item resides. Use the
-///   first-defined codebase in the configuration if nothing else is defined.
-/// * `language` - Optional language if different from the codebase's default.
+/// * `upstream` - Optional name of codebase in which item resides. Use the
+///   first-defined codebase in the configuration if not specified.
+/// * `rev` - Git revision, branch, or tag that the item was last reviewed.
+/// * `file` - Path to containing file, relative to the upstream's root.
 /// * `kind` - Type of object as per the codebase's objects of interest.
-/// * `identifier` - Name of the item being referenced.
-/// * `path` - Path to containing file, relative to the codebase's root.
-/// * `revision` - Git revision, branch, or tag that the item was last reviewed.
-/// * `ignore` - Optional, if `true`, ignore mismatches with the other codebase.
-/// * `state` - Optional, workflow state.
-///   TODO: Consider enum of Done, InProgress, NotStarted, WontImplement, Other(String)
-/// * `notes` - Optional human-friendly notes about the reference.
-/// * `hash_alg` - Optional hashing algorithm for contents. Default algorithm is
-///   `sha256`.
-/// * `salt` - Optional u64 salt for hash. Will look up in the database if not
-///   specified, and randomly initialized if not found.
-/// * `hash` - Optional hex string of salted hash.
+/// * `ident` - Name of the item being referenced.
+/// * `state` - Optional, workflow state. The built-in workflow uses `DONE` and `TODO`.
+/// * `action` - Optional, for implementation planning.
+/// * `notes` - Optional, human-friendly notes about the reference.
+/// * `ignore` - Optional, if `true`, ignore mismatches with the upstream codebase.
 #[proc_macro_attribute]
 pub fn rawr(_input: TokenStream, _annotated_item: TokenStream) -> TokenStream {
+    TokenStream::new()
+}
+
+/// Derive macro to allow use of rawr attribute inside enums and structs.
+#[proc_macro_derive(Rawr, attributes(rawr))]
+pub fn derive_helper_attr(_item: TokenStream) -> TokenStream {
     TokenStream::new()
 }
